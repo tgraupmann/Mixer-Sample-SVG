d073e7c20c6cde6df22ff5f9d4884e059f3e95a4
Timothy Graupmann <tim.graupmann@razer.com>
2019-09-10 22:31:27 -0700

    Added reference SVG

--- src/scripts.js
+++ src/scripts.js
@@ -8,6 +8,8 @@ window.addEventListener('load', function initMixer() {
     right: offset,
   });
 
+  /// INLINE SVG
+
   function accessInlineSVG() {
     var svgObject = document.getElementById('svgSampleInline');
     if (svgObject == undefined) {
@@ -43,16 +45,68 @@ window.addEventListener('load', function initMixer() {
   }
 
 
-  // TEST INLINE SVG
-  console.log('Showing Inline SVG in 3...2...1');
-  setupInlineSVG();
-  setTimeout(function() {
-    var svgObject = document.getElementById('svgSampleInline');
-    if (svgObject != undefined) {
-      svgObject.style = "";
-      accessInlineSVG();
+  /// REFERENCED SVG
+
+  function accessReferenceSVG() {
+    var svgObject = document.getElementById('svgSampleRef');
+    if (svgObject == undefined) {
+      console.error('accessReferenceSVG: Reference SVG: Object cannot be accessed!');
+      return;
     }
-  }, 3000);
+
+    var svgDoc = svgObject.contentDocument;
+    if (svgDoc == undefined) {
+      console.error('accessReferenceSVG: Reference SVG: Content Document cannot be accessed!');
+      return;
+    }
+    var leds = svgDoc.getElementsByClassName('led');
+    if (leds.length == 0) {
+      console.error('accessReferenceSVG: Reference SVG: Led cannot be accessed!');
+      return;
+    }
+    console.log('accessReferenceSVG: Reference SVG: leds', leds)
+  }
+
+  function setupReferenceSVG() {
+    var svgObject = document.getElementById('svgSampleRef');
+    if (svgObject == undefined) {
+      console.error('setupReferenceSVG: Reference SVG: Object cannot be accessed!');
+      return;
+    }
+
+    //load event doesn't fire because it's already loaded, probably
+    svgObject.addEventListener("load",function() {
+      console.log('setupReferenceSVG: Reference SVG: load event');
+      accessReferenceSVG();
+    }, false);
+  }
+
+
+  // TEST INLINE SVG
+  setupInlineSVG();
+  function testInlineSVG() {
+    console.log('Accessing Inline SVG in 3...2...1');
+    setTimeout(function() {
+      var svgObject = document.getElementById('svgSampleInline');
+      if (svgObject != undefined) {
+        accessInlineSVG();
+      }
+    }, 3000);
+  }
+  testInlineSVG();
+
+  // TEST REFERENCE SVG
+  setupReferenceSVG();
+  function testReferenceSVG() {
+    console.log('Showing Reference SVG in 3...2...1');
+    setTimeout(function() {
+      var svgObject = document.getElementById('svgSampleRef');
+      if (svgObject != undefined) {
+        svgObject.style = "";
+      }
+    }, 3000);
+  }
+  testReferenceSVG();
 
 
   mixer.isLoaded();
