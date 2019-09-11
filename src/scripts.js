6dcc06d3b09001ed605b6c64582c8f6e19a449bb
Timothy Graupmann <tim.graupmann@razer.com>
2019-09-11 09:54:07 -0700

    Added dynamic SVG test

--- src/scripts.js
+++ src/scripts.js
@@ -28,6 +28,8 @@ window.addEventListener('load', function initMixer() {
       return;
     }
     console.log('accessInlineSVG: Inline SVG: leds', leds)
+    // change color never gets called
+    leds[0].setAttribute("style", "fill: rgb(0,0,0)");
   }
 
   function setupInlineSVG() {
@@ -65,6 +67,8 @@ window.addEventListener('load', function initMixer() {
       return;
     }
     console.log('accessReferenceSVG: Reference SVG: leds', leds)
+    // change color to blue
+    leds[0].setAttribute("style", "fill: rgb(0,0,255)");
   }
 
   function setupReferenceSVG() {
@@ -81,6 +85,31 @@ window.addEventListener('load', function initMixer() {
   }
 
 
+  /// DYNAMIC SVG
+
+  function accessDynamicSVG() {
+    var svgObject = document.getElementById('svgSampleRef');
+    if (svgObject == undefined) {
+      console.error('accessDynamicSVG: Dynamic SVG: Object cannot be accessed!');
+      return;
+    }
+
+    var svgDoc = svgObject.contentDocument;
+    if (svgDoc == undefined) {
+      console.error('accessDynamicSVG: Dynamic SVG: Content Document cannot be accessed!');
+      return;
+    }
+    var leds = svgDoc.getElementsByClassName('led');
+    if (leds.length == 0) {
+      console.error('accessDynamicSVG: Dynamic SVG: Led cannot be accessed!');
+      return;
+    }
+    console.log('accessDynamicSVG: Dynamic SVG: leds', leds)
+    // change color to white
+    leds[0].setAttribute("style", "fill: rgb(255,255,255)");
+  }
+
+
   // TEST INLINE SVG
   setupInlineSVG();
   function testInlineSVG() {
@@ -94,6 +123,7 @@ window.addEventListener('load', function initMixer() {
   }
   testInlineSVG();
 
+
   // TEST REFERENCE SVG
   setupReferenceSVG();
   function testReferenceSVG() {
@@ -106,6 +136,40 @@ window.addEventListener('load', function initMixer() {
     }, 3000);
   }
   testReferenceSVG();
+
+
+  // TEST DYNAMIC SVG
+  function testDynamicSVG() {
+    var svgObject = document.getElementById('svgSampleDynamic');
+    if (svgObject == undefined) {
+      console.error('testDynamicSVG: Reference SVG: Object cannot be accessed!');
+      return;
+    }
+
+    // never gets called
+    svgObject.addEventListener("load",function() {
+      console.log('testDynamicSVG: Dynamic SVG: load event');
+
+    }, false);
+
+    var myCircle = document.createElementNS("http://www.w3.org/2000/svg","circle"); //to create a circle. for rectangle use "rectangle"
+    myCircle.setAttributeNS(null,"id","mycircle");
+    myCircle.setAttributeNS(null,"class","led");
+    myCircle.setAttributeNS(null,"cx",100);
+    myCircle.setAttributeNS(null,"cy",100);
+    myCircle.setAttributeNS(null,"r",50);
+    myCircle.setAttributeNS(null,"fill","cyan");
+    myCircle.setAttributeNS(null,"stroke","none");
+
+    svgObject.appendChild(myCircle);
+
+    accessDynamicSVG();
+
+    // regular DOM
+    svgObject.children[0].setAttribute("style", "fill: rgb(255,0,255)")
+    console.log('testDynamicSVG', 'class name', svgObject.children[0].className)
+  }
+  testDynamicSVG();
 
 
   mixer.isLoaded();
